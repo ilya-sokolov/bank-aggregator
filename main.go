6f2f0634499b71462faf27d03d08a149b96b07c5
Ilya Sokolov <developer.sokolov@gmail.com>
2020-09-03 03:19:10 +0300

    simple backend. AllRates and RateOwner rest API

--- main.go
+++ main.go
@@ -1,107 +1,29 @@
 package main
 
 import (
-	"./store"
-	"encoding/json"
-	"fmt"
-	"sync"
-
+	"github.com/gorilla/mux"
+	"github.com/ilya-sokolov/bank-aggregator/rest"
 	"net/http"
-	"time"
 )
-
-const (
-	tinkoffApi = "https://api.tinkoff.ru/v1/currency_rates"
-	sberApi    = "https://www.sberbank.ru/portalserver/proxy/?pipe=shortCachePipe&url=http%3A%2F%2Flocalhost%2Frates-web%2FrateService%2Frate%2Fcurrent%3FregionId%3D77%26rateCategory%3Dbase%26currencyCode%3D978%26currencyCode%3D840"
-	alfaApi    = "https://alfabank.ru/ext-json/0.2/exchange/cash?offset=0&limit=1&mode=rest"
-)
-
-var client = &http.Client{Timeout: 10 * time.Second}
-
-func getTinkoffRate(from, to string) (*store.Rate, error) {
-	r, err := client.Get(tinkoffApi + "?from=" + from + "&to=" + to)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-	tr := &store.TinkoffRate{}
-	err = json.NewDecoder(r.Body).Decode(tr)
-	if err != nil {
-		return nil, err
-	}
-	return store.MakeFromTinkoff(tr), nil
-}
-
-func getSberRate(currency string, amount int) (*store.Rate, error) {
-	r, err := client.Get(sberApi)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-	sr := &store.SberRate{}
-	err = json.NewDecoder(r.Body).Decode(sr)
-	if err != nil {
-		return nil, err
-	}
-	return store.MakeFromSber(sr, amount, currency), nil
-}
-
-func getAlfaRate(currency string) (*store.Rate, error) {
-	r, err := client.Get(alfaApi)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-	ar := &store.AlfaRate{}
-	err = json.NewDecoder(r.Body).Decode(ar)
-	if err != nil {
-		return nil, err
-	}
-	return store.MakeFromAlfa(ar, currency), nil
-}
-
-func getAllRates(currency string) (rates []store.Rate) {
-	wg := sync.WaitGroup{}
-	wg.Add(3)
-	go func() {
-		rate, err := getTinkoffRate(currency, store.RUB)
-		if err != nil {
-			return
-		}
-		rates = append(rates, *rate)
-		wg.Done()
-	}()
-	go func() {
-		rate, err := getSberRate(currency, 100)
-		if err != nil {
-			return
-		}
-		rates = append(rates, *rate)
-		wg.Done()
-	}()
-	go func() {
-		rate, err := getAlfaRate(currency)
-		if err != nil {
-			return
-		}
-		rates = append(rates, *rate)
-		wg.Done()
-	}()
-	wg.Wait()
-	return rates
-}
 
 func main() {
-	for {
-		printAllRates()
-		time.Sleep(10 * time.Second)
+	router := mux.NewRouter()
+	router.HandleFunc("/api/v1/rates", rest.AllRates).Methods("GET")
+	router.HandleFunc("/api/v1/rate", rest.RateOwner).Methods("GET")
+	err := http.ListenAndServe(":8090", router)
+	if err != nil {
+		panic(err)
 	}
+	//for {
+	//	printAllRates()
+	//	time.Sleep(10 * time.Second)
+	//}
 }
 
-func printAllRates() {
-	rate := getAllRates(store.USD)
-	for _, r := range rate {
-		fmt.Printf("%s: %s -> %s BUY: %.2f  SELL: %.2f\n", r.Owner, r.FromCurrency, r.ToCurrency, r.Buy, r.Sell)
-	}
-	fmt.Println("<========================>")
-}
+//func printAllRates() {
+//	rate := getAllRates()
+//	for _, r := range rate {
+//		fmt.Printf("%s: %s -> %s BUY: %.2f  SELL: %.2f\n", r.Owner, r.FromCurrency, r.ToCurrency, r.Buy, r.Sell)
+//	}
+//	fmt.Println("<========================>")
+//}
---
+++ rest/api.go
@@ -0,0 +1,122 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ilya-sokolov/bank-aggregator/store"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const (
+	tinkoffApi = "https://api.tinkoff.ru/v1/currency_rates"
+	sberApi    = "https://www.sberbank.ru/portalserver/proxy/?pipe=shortCachePipe&url=http%3A%2F%2Flocalhost%2Frates-web%2FrateService%2Frate%2Fcurrent%3FregionId%3D77%26rateCategory%3Dbase%26currencyCode%3D978%26currencyCode%3D840"
+	alfaApi    = "https://alfabank.ru/ext-json/0.2/exchange/cash?offset=0&limit=1&mode=rest"
+)
+
+var client = &http.Client{Timeout: 10 * time.Second}
+
+func getTinkoffRate(from, to string) (*store.Rate, error) {
+	r, err := client.Get(tinkoffApi + "?from=" + from + "&to=" + to)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	tr := &store.TinkoffRate{}
+	err = json.NewDecoder(r.Body).Decode(tr)
+	if err != nil {
+		return nil, err
+	}
+	return store.MakeFromTinkoff(tr), nil
+}
+
+func getSberRate(currency string, amount int) (*store.Rate, error) {
+	r, err := client.Get(sberApi)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	sr := &store.SberRate{}
+	err = json.NewDecoder(r.Body).Decode(sr)
+	if err != nil {
+		return nil, err
+	}
+	return store.MakeFromSber(sr, amount, currency), nil
+}
+
+func getAlfaRate(currency string) (*store.Rate, error) {
+	r, err := client.Get(alfaApi)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	ar := &store.AlfaRate{}
+	err = json.NewDecoder(r.Body).Decode(ar)
+	if err != nil {
+		return nil, err
+	}
+	return store.MakeFromAlfa(ar, currency), nil
+}
+
+func AllRates(w http.ResponseWriter, r *http.Request) {
+	rates := getAllRates(store.USD)
+	for _, r := range rates {
+		fmt.Printf("%s: %s -> %s BUY: %.2f  SELL: %.2f\n", r.Owner, r.FromCurrency, r.ToCurrency, r.Buy, r.Sell)
+	}
+}
+
+func RateOwner(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	owner := query.Get("owner")
+	currency := query.Get("currency")
+	if currency == "" {
+		currency = store.USD
+	}
+	rate := &store.Rate{}
+	var err error
+	switch owner {
+	case "tinkoff":
+		rate, err = getTinkoffRate(currency, store.RUB)
+	case "sber":
+		rate, err = getSberRate(currency, 100)
+	case "alfa":
+		rate, err = getAlfaRate(currency)
+	}
+	if err != nil {
+		http.Error(w, "Field \"owner\" has empty.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%s: %s -> %s BUY: %.2f  SELL: %.2f\n", rate.Owner, rate.FromCurrency, rate.ToCurrency, rate.Buy, rate.Sell)
+}
+
+func getAllRates(currency string) (rates []store.Rate) {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go func() {
+		rate, err := getTinkoffRate(currency, store.RUB)
+		if err != nil {
+			return
+		}
+		rates = append(rates, *rate)
+		wg.Done()
+	}()
+	go func() {
+		rate, err := getSberRate(currency, 100)
+		if err != nil {
+			return
+		}
+		rates = append(rates, *rate)
+		wg.Done()
+	}()
+	go func() {
+		rate, err := getAlfaRate(currency)
+		if err != nil {
+			return
+		}
+		rates = append(rates, *rate)
+		wg.Done()
+	}()
+	wg.Wait()
+	return rates
+}
